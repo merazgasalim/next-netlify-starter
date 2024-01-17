967910fefccd4470c4a6e38aa13094777d1c3dd7
Salim Merazga <merazgasalim@hotmail.fr>
2024-01-17 21:29:03 +0100

    paypal

--- components/Plans.js
+++ components/Plans.js
@@ -19,12 +19,6 @@ import { FaCheckCircle } from "react-icons/fa";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { useState } from "react";
 
-const initialOptions = {
-  clientId: "test",
-  currency: "USD",
-  intent: "capture",
-};
-
 function PriceWrapper(props) {
   const { children } = props;
 
@@ -45,97 +39,97 @@ export default function Plans() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [plan, setPlan] = useState();
   return (
-    <PayPalScriptProvider options={initialOptions}>
-      <Container as={"section"} maxW="full" textAlign={"center"} id="plans">
-        <Heading as={"h2"}>Choose Your Perfect Plan</Heading>
+    <Container as={"section"} maxW="full" textAlign={"center"} id="plans">
+      <Heading as={"h2"}>Choose Your Perfect Plan</Heading>
 
-        <Text>
-          Enjoy unlimited access to the world of entertainment. Kick back with
-          full seasons of exclusive Originals, the mega-hit movie library and
-          more.
-        </Text>
+      <Text>
+        Enjoy unlimited access to the world of entertainment. Kick back with
+        full seasons of exclusive Originals, the mega-hit movie library and
+        more.
+      </Text>
 
-        <SimpleGrid
-          // direction={{ base: "column", md: "row" }}
-          columns={{ base: 1, md: 2, lg: 4 }}
-          textAlign="center"
-          justify="center"
-          spacing={{ base: 4, lg: 10 }}
-          py={10}
-          w={"full"}
-        >
-          {Offers.map((plan, idx) => (
-            <PriceWrapper key={idx}>
-              <Box py={4} px={{ base: 2, md: 12 }}>
-                <Text fontWeight="500" fontSize="2xl">
-                  {plan.duration}
+      <SimpleGrid
+        // direction={{ base: "column", md: "row" }}
+        columns={{ base: 1, md: 2, lg: 4 }}
+        textAlign="center"
+        justify="center"
+        spacing={{ base: 4, lg: 10 }}
+        py={10}
+        w={"full"}
+      >
+        {Offers.map((plan, idx) => (
+          <PriceWrapper key={idx}>
+            <Box py={4} px={{ base: 2, md: 12 }}>
+              <Text fontWeight="500" fontSize="2xl">
+                {plan.duration}
+              </Text>
+              <HStack justifyContent="center">
+                <Text fontSize="3xl" fontWeight="600">
+                  $
                 </Text>
-                <HStack justifyContent="center">
-                  <Text fontSize="3xl" fontWeight="600">
-                    $
-                  </Text>
-                  <Text fontSize="5xl" fontWeight="900">
-                    {plan.price}
-                  </Text>
-                  <Text fontSize="3xl" color="gray.500">
-                    /month
-                  </Text>
-                </HStack>
-              </Box>
-              <VStack
-                bg={useColorModeValue("gray.50", "gray.700")}
-                py={4}
-                borderBottomRadius={"xl"}
-              >
-                <List spacing={3} textAlign="start" px={{ base: 2, md: 12 }}>
-                  <ListItem>
-                    <ListIcon as={FaCheckCircle} color="green.500" />
-                    Up to 15,000 Channels
-                  </ListItem>
-                  <ListItem>
-                    <ListIcon as={FaCheckCircle} color="green.500" />
-                    Up to 40,000 Vods
-                  </ListItem>
-                  <ListItem>
-                    <ListIcon as={FaCheckCircle} color="green.500" />
-                    8K / 4K Picture Quality
-                  </ListItem>
-                  <ListItem>
-                    <ListIcon as={FaCheckCircle} color="green.500" />
-                    Without Freezing
-                  </ListItem>
-                  <ListItem>
-                    <ListIcon as={FaCheckCircle} color="green.500" />
-                    99.9% Server Uptime
-                  </ListItem>
-                  <ListItem>
-                    <ListIcon as={FaCheckCircle} color="green.500" />
-                    24/7 Support
-                  </ListItem>
-                </List>
-                <VStack w="80%" pt={7}>
-                  <Button w="full" colorScheme="red" variant="outline">
-                    Start trial
-                  </Button>
-                  <Button
-                    w="full"
-                    colorScheme="red"
-                    onClick={() => {
-                      setPlan(plan);
-                      onOpen();
-                    }}
-                  >
-                    Subscribe
-                  </Button>
-                </VStack>
+                <Text fontSize="5xl" fontWeight="900">
+                  {plan.price}
+                </Text>
+                <Text fontSize="3xl" color="gray.500">
+                  /month
+                </Text>
+              </HStack>
+            </Box>
+            <VStack
+              bg={useColorModeValue("gray.50", "gray.700")}
+              py={4}
+              borderBottomRadius={"xl"}
+            >
+              <List spacing={3} textAlign="start" px={{ base: 2, md: 12 }}>
+                <ListItem>
+                  <ListIcon as={FaCheckCircle} color="green.500" />
+                  Up to 15,000 Channels
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={FaCheckCircle} color="green.500" />
+                  Up to 40,000 Vods
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={FaCheckCircle} color="green.500" />
+                  8K / 4K Picture Quality
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={FaCheckCircle} color="green.500" />
+                  Without Freezing
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={FaCheckCircle} color="green.500" />
+                  99.9% Server Uptime
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={FaCheckCircle} color="green.500" />
+                  24/7 Support
+                </ListItem>
+              </List>
+              <VStack w="80%" pt={7}>
+                <Button w="full" colorScheme="red" variant="outline">
+                  Start trial
+                </Button>
+                <Button
+                  w="full"
+                  colorScheme="red"
+                  onClick={() => {
+                    setPlan(plan);
+                    onOpen();
+                  }}
+                >
+                  Subscribe
+                </Button>
               </VStack>
-            </PriceWrapper>
-          ))}
-        </SimpleGrid>
+            </VStack>
+          </PriceWrapper>
+        ))}
+      </SimpleGrid>
 
+      {isOpen && plan && (
         <PayPal isOpen={isOpen} onClose={onClose} plan={plan} />
-      </Container>
-    </PayPalScriptProvider>
+      )}
+    </Container>
   );
 }
 
--- components/general/PayPal.js
+++ components/general/PayPal.js
@@ -9,9 +9,124 @@ import {
   Button,
   HStack,
   Text,
-  Heading,
+  Spinner,
+  useToast,
 } from "@chakra-ui/react";
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
+import { TbDownloadOff } from "react-icons/tb";
+
+const ButtonWrapper = ({ plan }) => {
+  const toast = useToast();
+  const [{ isPending, isResolved, isRejected }] = usePayPalScriptReducer();
+
+  const getAmount = () => {
+    let GTotal = plan.price;
+    return {
+      currency_code: "USD",
+      value: GTotal,
+      breakdown: {
+        item_total: {
+          currency_code: "USD",
+          value: GTotal,
+        },
+        // shipping: {
+        //   value: shippingCost / 100,
+        //   currency_code: "USD",
+        // },
+        discount: {
+          value: "0.00",
+          currency_code: "USD",
+        },
+      },
+    };
+  };
+
+  const getItems = () => {
+    let items = [
+      {
+        name: `${plan.duration} IPTV Subscription`,
+        //sku: item.sku,
+        unit_amount: {
+          currency_code: "USD",
+          value: plan.price,
+        },
+        quantity: 1,
+        description: `${plan.duration} IPTV Subscription`,
+        category: "DIGITAL_GOODS",
+      },
+    ];
+
+    //console.log(items);
+    return items;
+  };
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: getAmount(),
+          description: `${plan.duration} IPTV Subscription`,
+          items: getItems(),
+          soft_descriptor: "incolive",
+        },
+      ],
+
+      application_context: {
+        brand_name: "incolive",
+        shipping_preference: "NO_SHIPPING", // default is "GET_FROM_FILE"
+      },
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.get().then(function (details) {
+      const body = {
+        payPalDetails: details,
+      };
+
+      return fetch(`/api/orders`, {
+        method: "PUT",
+        body: JSON.stringify(body),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          if (res.success) {
+            afterSuccess();
+          } else {
+            toast({
+              description: res.reason,
+              status: "warning",
+              //duration: 9000,
+              isClosable: true,
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  };
+
+  const onError = (err) => {
+    console.log(err.toString());
+  };
+
+  if (isPending) return <Spinner />;
+  if (isRejected) return <TbDownloadOff color="tomato" />;
+  return (
+    <PayPalButtons
+      style={{ layout: "vertical", shape: "pill" }}
+      createOrder={createOrder}
+      onApprove={onApprove}
+      onError={onError}
+      //forceReRender={[nbrUits]}
+    />
+  );
+};
 
 export default function PayPal({ isOpen, onClose, plan }) {
   console.log(plan);
@@ -23,6 +138,7 @@ export default function PayPal({ isOpen, onClose, plan }) {
       isCentered
       scrollBehavior="inside"
       closeOnOverlayClick={false}
+      size="lg"
     >
       <ModalOverlay />
       <ModalContent>
@@ -33,7 +149,15 @@ export default function PayPal({ isOpen, onClose, plan }) {
             <Text>{plan?.duration} </Text>
             <Text>${plan?.price}</Text>
           </HStack>
-          <PayPalButtons style={{ layout: "horizontal" }} />
+          <PayPalScriptProvider
+            options={{
+              clientId: "test",
+              components: "buttons",
+              currency: "USD",
+            }}
+          >
+            <ButtonWrapper showSpinner={false} plan={plan} />
+          </PayPalScriptProvider>
         </ModalBody>
         <ModalFooter>
           <Button colorScheme="blue" mr={3} onClick={onClose}>
