5500b93aba23699713f63ca6ea95edd3ece7c08c
Salim Merazga <merazgasalim@hotmail.fr>
2024-01-22 20:05:41 +0100

    mongo

--- pages/slice-simulator.js
+++ pages/slice-simulator.js
@@ -1,14 +1,17 @@
 "use client";
 
-import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
-import { SliceZone } from "@prismicio/react";
+//import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
+//import { SliceZone } from "@prismicio/react";
 
-import { components } from "../slices";
+//import { components } from "slices";
+
 
 export default function SliceSimulatorPage() {
   return (
-    <SliceSimulator
+    <>
+    {/*<SliceSimulator
       sliceZone={(props) => <SliceZone {...props} components={components} />}
-    />
+  />*/}
+    </>
   );
 }
