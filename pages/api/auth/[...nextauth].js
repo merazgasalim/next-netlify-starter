222e045645bc20a989a03bb6da44f9b6502f651a
Salim Merazga <merazgasalim@hotmail.fr>
2024-01-27 10:41:42 +0100

    login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 
 import clientPromise from "lib/mongoClient";
-import { ObjectId } from "mongodb";
+const { ObjectId } = require("mongodb");
 
 export const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
@@ -17,14 +17,17 @@ export const authOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        console.log(credentials.email);
+      
         // Add logic to verify credentials here
         if (!credentials) return null;
+        const { email, password } = credentials;
+
         const MongoClient = await clientPromise;
-        const user = await MongoClient.db()
+        console.log(credentials);
+        const user = await MongoClient.db(process.env.DB_NAME)
           .collection("customers")
           .findOne({
-            _id: new ObjectId(credentials.email),
+            _id: email,
           });
         console.log(user, "oo");
         // await clientPromise.then(async (MongoClient) => {
@@ -35,11 +38,11 @@ export const authOptions = {
         //   console.log(user, "oo");
         // });
 
-        const { email, password } = credentials;
+        
         // Fetch user and password hash from your database
         // Example: const user = await getUserByEmail(email)
-        if (user && bcrypt.compareSync(password, user.passwordHash)) {
-          return { id: user.id, name: user.name, email: user.email };
+        if (user && bcrypt.compareSync(password, user.password)) {
+          return { email: user._id };
         } else {
           throw new Error("Invalid credentials");
         }
@@ -59,6 +62,7 @@ export const authOptions = {
   pages: {
     signIn: "/auth/signIn", // Custom sign-in page
   },
+  debug: true,
 };
 
 export default NextAuth(authOptions);
